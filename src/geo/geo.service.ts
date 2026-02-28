cfc9da1123b8948bc8d722d9d6d07ed022a32157
Angel Gonzalez <angel.gonzalez@zascita.com>
2026-02-27 18:35:08 -0600

    geo-service

--- src/geo/geo.service.ts
+++ src/geo/geo.service.ts
@@ -1,24 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { EventPattern, Payload, Ctx, MqttContext } from '@nestjs/microservices';
-import { GeoGateway } from './geo.gateway';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Injectable()
 export class GeoService {
-  constructor(private readonly geoGateway: GeoGateway) {}
-
-  @EventPattern('usuarios/#')
-  handleLocationUpdate(
-    @Payload() data: any,
-    @Ctx() context: MqttContext,
-  ) {
-    const topic = context.getTopic();
-
-    console.log('📥 Topic:', topic);
+  @MessagePattern('usuarios/+/ubicacion')
+  handleUbicacion(@Payload() data: any) {
     console.log('📍 Ubicación recibida:', data);
-
-    // Solo procesamos si termina en /ubicacion
-    if (topic.endsWith('/ubicacion')) {
-      this.geoGateway.emitLocation(data);
-    }
   }
 }
