3e02ed21609c9956535eac14454e13a34ebf968c
Angel Gonzalez <angel.gonzalez@zascita.com>
2026-02-28 23:33:40 -0600

    260226 5

--- src/mqtt.listener.ts
+++ src/mqtt.listener.ts
@@ -4,18 +4,25 @@ import { EventPattern, Payload, Ctx, MqttContext } from '@nestjs/microservices';
 @Controller()
 export class MqttListener {
 
-  @EventPattern('#') // escucha TODO
-  handleAll(
+  @EventPattern('usuarios/+/ubicacion')
+  handleLocation(
     @Payload() data: any,
     @Ctx() context: MqttContext,
   ) {
 
     const topic = context.getTopic();
 
+    // obtener userId del topic
+    const parts = topic.split('/');
+    const userId = parts[1];
+
     console.log('======================');
-    console.log('📥 MQTT RECIBIDO');
-    console.log('Topic:', topic);
-    console.log('Data:', data);
+    console.log('📍 UBICACION RECIBIDA');
+    console.log('Usuario:', userId);
+    console.log('Lat:', data.lat);
+    console.log('Lng:', data.lng);
     console.log('======================');
+
+    // aquí después guardarás en DB
   }
 }
